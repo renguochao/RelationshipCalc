67caf495c76b22bc26579e518652200155710bcf
010726 <renguochao@htsc.com>
2018-01-29 13:49:34 +0800

    Initial Commit

---
+++ RelationshipCalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RelationshipCalc
+//
+//  Created by Guochao Ren on 2018/1/29.
+//  Copyright © 2018年 KS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RelationshipCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RelationshipCalc
+//
+//  Created by Guochao Ren on 2018/1/29.
+//  Copyright © 2018年 KS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
