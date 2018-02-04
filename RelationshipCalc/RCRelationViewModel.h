7d111d54caf5d7dbbb0fea3f2048f134a9a8692a
010726 <renguochao@htsc.com>
2018-02-04 20:36:44 +0800

    feat: add RCRelationViewModel

---
+++ RelationshipCalc/RCRelationViewModel.h
@@ -0,0 +1,13 @@
+//
+//  RCRelationViewModel.h
+//  RelationshipCalc
+//
+//  Created by Guochao Ren on 2018/2/4.
+//  Copyright © 2018年 KS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCRelationViewModel : NSObject
+
+@end
---
+++ RelationshipCalc/RCRelationViewModel.m
@@ -0,0 +1,13 @@
+//
+//  RCRelationViewModel.m
+//  RelationshipCalc
+//
+//  Created by Guochao Ren on 2018/2/4.
+//  Copyright © 2018年 KS. All rights reserved.
+//
+
+#import "RCRelationViewModel.h"
+
+@implementation RCRelationViewModel
+
+@end
