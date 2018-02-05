382325bc7c88e0b04fc6d333e6e7d766e3d336b0
010726 <renguochao@htsc.com>
2018-02-05 16:24:32 +0800

    feat(#MERCURY-981#): ViewModel添加接口

--- RelationshipCalc/RCRelationViewModel.h
+++ RelationshipCalc/RCRelationViewModel.h
@@ -10,4 +10,24 @@
 
 @interface RCRelationViewModel : NSObject
 
++ (instancetype)sharedInstance;
+
+/**
+ 将输入字符串转换成关系链
+ ex:
+ "我的哥哥的妈妈的爸爸的儿子" -> ",ob,m,f,s"
+ 
+ @param inputString 输入字符串
+ @return 关系链
+ */
+- (NSString *)transformInputStringToRelationKey:(NSString *)inputString;
+
+/**
+ 合并冗余的关系，简化关系链
+
+ @param relationChain 关系链字符串
+ @return 简化后的关系链
+ */
+- (NSString *)simplifyRelationChain:(NSString *)relationChain;
+
 @end
--- RelationshipCalc/RCRelationViewModel.m
+++ RelationshipCalc/RCRelationViewModel.m
@@ -8,6 +8,83 @@
 
 #import "RCRelationViewModel.h"
 
+@interface RCRelationViewModel ()
+
+/**
+ 关系链-称谓映射字典
+ */
+@property (nonatomic, strong) NSDictionary<NSString *, NSArray<NSString *> *> *dictRcMapping;
+
+/**
+ 简化关系链使用的正则表达式
+ */
+@property (nonatomic, strong) NSArray<NSDictionary<NSString *, NSString *> *> *arrFilter;
+
+
+@end
+
 @implementation RCRelationViewModel
+
++ (instancetype)sharedInstance {
+    static dispatch_once_t onceToken;
+    static RCRelationViewModel *instance = nil;
+    dispatch_once(&onceToken, ^{
+        instance = [RCRelationViewModel new];
+    });
+    return instance;
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        NSString *dataFilePath = [[NSBundle mainBundle] pathForResource:@"data" ofType:@"json"];
+        NSString *filterFilePath = [[NSBundle mainBundle] pathForResource:@"filter" ofType:@"json"];
+        
+        NSData *rcMappingData = [NSData dataWithContentsOfFile:dataFilePath];
+        NSData *filterData = [NSData dataWithContentsOfFile:filterFilePath];
+        
+        NSError *error = nil;
+        _dictRcMapping = [NSJSONSerialization JSONObjectWithData:rcMappingData options:NSJSONReadingAllowFragments error:&error];
+        _arrFilter = [NSJSONSerialization JSONObjectWithData:filterData options:NSJSONReadingAllowFragments error:&error][@"filter"];
+        
+    }
+    return self;
+}
+
+- (NSString *)transformInputStringToRelationKey:(NSString *)inputString {
+    __block NSString *result = @"";
+    
+    if (!inputString || inputString.length == 0) {
+        return result;
+    }
+    
+    NSArray *inputComponents = [inputString componentsSeparatedByString:@"的"];
+    for (NSString *comp in inputComponents) {
+        [self.dictRcMapping enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, NSArray<NSString *> * _Nonnull obj, BOOL * _Nonnull stop) {
+            for (NSString *str in obj) {
+                // 从关系链-称谓映射表中查找对应的key（过滤“我”）
+                if ([comp isEqualToString:str] &&
+                    ![key isEqualToString:@""]) {
+                    result = [result stringByAppendingString:[NSString stringWithFormat:@",%@", key]];
+                    *stop = YES;
+                    break;
+                }
+            }
+        }];
+    }
+    
+    return result;
+}
+
+- (NSString *)simplifyRelationChain:(NSString *)relationChain {
+    
+    
+    return nil;
+}
+
+
+
+
+
+
 
 @end
