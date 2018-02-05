9a1b93cc05a8354bf1dff9d0ca2496c3a419c6fa
010726 <renguochao@htsc.com>
2018-02-05 16:25:28 +0800

    feat(#MERCURY-981#): 添加单元测试scheme和用例

---
+++ RelationshipCalcTests/RCRelationViewModelTestCase.m
@@ -0,0 +1,40 @@
+//
+//  RCRelationViewModelTestCase.m
+//  RelationshipCalcTests
+//
+//  Created by Guochao Ren on 2018/2/4.
+//  Copyright © 2018年 KS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "RCRelationViewModel.h"
+
+@interface RCRelationViewModelTestCase : XCTestCase
+
+@property (nonatomic, strong) RCRelationViewModel *viewModel;
+
+@end
+
+@implementation RCRelationViewModelTestCase
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.viewModel = [RCRelationViewModel sharedInstance];
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testTransformInputStringToRelationKey {
+    NSString *str1 = @"我的哥哥的妈妈的爸爸的儿子";
+    NSString *str2 = @"儿子的妈妈";
+    
+    XCTAssertTrue([[self.viewModel transformInputStringToRelationKey:str1] isEqualToString:@",ob,m,f,s"]);
+    XCTAssertTrue([[self.viewModel transformInputStringToRelationKey:str2] isEqualToString:@",s,m"]);
+
+}
+
+@end
